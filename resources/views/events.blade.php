e4e7a252399d52419f1d4ca6f96ae0e189cbf2dc
Danijel Martinek <danijel.martinek.4@gmail.com>
2017-10-25 16:57:24 +0200

    designing events page

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -5,51 +5,182 @@
 <div class="container">
   <div class="card row">
   <div class="card-image waves-effect waves-block waves-light col s12 m6 l6">
-    <img class="activator" src="https://cdn.shopify.com/s/files/1/0187/6812/products/Bamboo_c36ce56d-5752-4e9e-adab-59c3fae06ac2.jpg?v=1502751522">
+    <img class="activator" src="https://1.img-dpreview.com/files/p/E~TS590x0~articles/1345361295/gx850_main.jpeg">
   </div>
   <div class="col s12 m6 l6">
     <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">Dani Karijera Križevci</span>
+      <span class="card-title activator grey-text text-darken-4 center events-main-card-title">Dani Karijera Križevci</span>
       <div class="card-content">
         <p>I am a very simple card. I am good at containing small bits of information.</p>
       </div>
     </div>
-  <h3 id="countdown" class="center"></h3>
+  <div class="card-action"><h4 id="countdown" class="center"></h4></div>
   </div>
 </div>
-
-  <div><blockquote><h3 class="uppercase">Novosti</h3></blockquote></div>
+  <br>
+  <div><blockquote><h4 class="uppercase">Nadolazeći događaji</h4></blockquote></div>
   <div class="center">
     <div class="row">
 
-      <span class="col s12 m7 l8">
-        <span class="col s12 m5 l5">
-          <form>
-            <div class="input-field">
-              <input id="search" type="search" required>
-              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-              <i class="material-icons">close</i>
-            </div>
-          </form>
-        </span>
+<span class="col s12 m8 l8 left">
+<ul class="collapsible" data-collapsible="accordion">
+
+    <li>
+      <div class="collapsible-header"><h5>2. Križevačka Tehnološka Večer</h5></div>
+      <div class="collapsible-body">
+        <div class="row">
+
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">place</i>Knjižnica "Franjo Marković", Trg sv. Florijana 14, Križevci</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">event</i>5. kolovoz 2017.</span>
+            <br>
+            <span><i class="material-icons material-icons-works">schedule</i>18:00</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">record_voice_over</i>razni predavači</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">attach_money</i>besplatno</span>
+          </div>
+
+          <div class="col s12 m12 l12"><br></div>
+          <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
+          <div class="col s12 m12 l12"><br></div>
+
+          <div class="col s12 m12 l12">
+            <span>
+              1. Križevačka Tehnološka Večer je bila pun pogodak. Imali smo vrlo zanimljive predavače koji su uspjeli animirati i zainteresirati publiku za razne tehnologije, ali i za cijelu inicijativu.
+            Posjećenost je bila odlična i dojmovi su samo pozitivni.
+            <br><br>
+            Pozivamo vas na drugu večer poslovno-tehnološkog događaja u Križevcima, pod nazivom "Križevačke Tehnološke Večeri" u sklopu inicijative zajednice informatičara za edukaciju i otvaranje radnih mjesta u novom tehnološkom parku u centru grada.
+            Na događaju će nastupiti ljudi iz raznih područja tehnologije i različitih poduzeća koji će ukratko predstaviti tehnologije kojima se bave te tako pokušati privući što više interesa za spomenutu tehnologiju ili područje.
+            <br><br>
+            Ovu će subotu pričati ljudi koji rade ili su radili u Poslovnoj inteligenciji, Fiveu i Iskonu. Pričat će se o mrežama i routerima, razvoju analitičkih sustava u Poslovnoj inteligenciji, startup i event managementu te Unity game developmentu. Također će se složiti unikatna tribina kojom će se potaknuti višesmjerna komunikacija između publike, govornika i ljudi iz inicijative.
+            <br><br>
+            Nastupit će:<br>
+            Mario Martinek<br>
+            Dominik Pavičić<br>
+            Marko Katanović<br>
+            Tomislav Tenodi<br>
+            Tomislav Novosel
+            <br><br>
+            Moderatori će biti:<br>
+            Danijel Martinek<br>
+            Filip Kesteli
+            <br><br>
+            Pod pokroviteljstvom Grada Križevaca i suorganizacija ureda gradonačelnika.
+            </span></div>
+
+            <div class="col s12 m12 l12"><br></div>
+            <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
 
 
-        <div class="col s12 m12 l12">
-          <div class="divider divider-margin"></div>
+              <img class="materialboxed events-thumbnail col s6 m4 l3" width="150" src="https://www.krizevci.info/wp-content/uploads/2017/07/IT_vecer-8-Krizevacke-tehnoloske-veceri-by-Irma-Vargic.jpg">
+
         </div>
-      </span>
+      </div>
+    </li>
+
+    <li>
+      <div class="collapsible-header"><h5>Microsoft & .NET konferencija</h5></div>
+      <div class="collapsible-body">
+        <div class="row">
+
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">place</i>Tehnološki Park, ulica Franje Tuđmana 20, Križevci</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">event</i>nepoznato</span>
+            <br>
+            <span><i class="material-icons material-icons-works">schedule</i>nepoznato</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">record_voice_over</i> Goran Žarinac <br> (Končar d.d.) </span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">attach_money</i>250 kn</span>
+          </div>
+
+          <div class="col s12 m12 l12"><br></div>
+          <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
+          <div class="col s12 m12 l12"><br></div>
+
+          <div class="col s12 m12 l12">
+            <span>
+            Pridružite nam se u programiranju aplikacija svaki dan poslije 10 sati. Tehnologije koje trenutno koristimo su
+            C, C++, Java, Javascript, html, css i drugo. Izražujemo web i mobilne aplikacije.
+            </span></div>
+
+            <div class="col s12 m12 l12"><br></div>
+            <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
+
+        </div>
+      </div>
+    </li>
+
+  </ul>
+
+  <h5 class="uppercase col s12 m12 l12 inline-block left">Prošli</h5>
+
+  <ul class="collapsible col s12 m12 l12 left" data-collapsible="accordion">
+
+    <li>
+      <div class="collapsible-header"><h5>1. Križevačka Tehnološka Večer</h5></div>
+      <div class="collapsible-body">
+        <div class="row">
+
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">place</i>Knjižnica "Franjo Marković", Trg sv. Florijana 14, Križevci</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">event</i>29. srpnja 2017.</span>
+            <br>
+            <span><i class="material-icons material-icons-works">schedule</i>18:00</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">record_voice_over</i>razni predavači</span>
+          </div>
+          <div class="col s12 m3 l3 center">
+            <span><i class="material-icons material-icons-works">attach_money</i>besplatno</span>
+          </div>
+
+          <div class="col s12 m12 l12"><br></div>
+          <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
+          <div class="col s12 m12 l12"><br></div>
+
+          <div class="col s12 m12 l12">
+            <span>
+            U subotu, 29. srpnja 2017. godine u prepunoj multimedijskoj dvorani Gradske knjižnice Franjo Marković Križevci održana je
+            1. Križevačka tehnološka večer pod pokroviteljstvom Grada Križevaca.
+            Na samom početku okupljene je pozdravio križevački gradonačelnik Mario Rajn istaknuvši zadovoljstvo pokretanjem ovog događaja:<br>
+            – Tehnološki park koji je izgrađen namijenjen je vama, a vjerujem da će jedna tehnološka večer biti održana i u Razvojnom centru i tehnološkom parku.<br>
+            <br><br>
+            Kao predavači nastupili su Filip Drašić koji je ukratko predstavio svoju firmu Ericsson Nikola Tesla, Tomislav Tenodi koji je govorio o svojem putu do otvaranja vlastite tvrtke,
+            Tino Balint rekavši svoju viziju "Tehnološkog Parka" te Filip Kesteli koji je predstavio neke od planova i projekata inicijative.
+            <br><br>
+            Uz gradonačelnika Rajna, događaju su prisustvovali zamjenik gradonačelnika Danijel Šaško, pročelnik Upravnog odjela za gospodastvo i financije Darko Masnec, pročelnik Upravnog odjela za društvene
+            djelatnosti Mladen Tenodi te predsjednik Gradskog vijeća Grada Križevaca Marko Katanović.
+            <br><br>
+            Foto: Irma Vargić
+            </span></div>
+
+            <div class="col s12 m12 l12"><br></div>
+            <div class="col s12 m12 l12" style="opacity: 0.2;"><hr></div>
+
+
+              <img class="materialboxed events-thumbnail col s6 m4 l3" width="150" src="https://www.krizevci.info/wp-content/uploads/2017/07/IT_vecer-14-Krizevacke-tehnoloske-veceri-by-Irma-Vargic.jpg">
+              <img class="materialboxed events-thumbnail col s6 m4 l3" width="150" src="https://www.krizevci.info/wp-content/uploads/2017/07/IT_vecer-12-Krizevacke-tehnoloske-veceri-by-Irma-Vargic.jpg">
+              <img class="materialboxed events-thumbnail col s6 m4 l3" width="150" src="https://www.krizevci.info/wp-content/uploads/2017/07/IT_vecer-8-Krizevacke-tehnoloske-veceri-by-Irma-Vargic.jpg">
+        </div>
+      </div>
+    </li>
+
+  </ul>
+</span>
 
       <span class="col s12 m4 l4">
-        <span class="col s12 m12 l12">
-          <div class="card-panel">
-              <div class="card-image"><img class="ad_container" src="http://hamuraagency.com/dist/frontend/images/icon/advertising.png"></div>
-          </div>
-        </span>
-        <span class="col s12 m12 l12">
-          <div class="card-panel">
-              <div class="card-image"><img class="ad_container" src="http://hamuraagency.com/dist/frontend/images/icon/advertising.png"></div>
-          </div>
-        </span>
         <span class="col s12 m12 l12">
           <div class="card-panel">
               <div class="card-image"><img class="ad_container" src="http://hamuraagency.com/dist/frontend/images/icon/advertising.png"></div>
@@ -65,7 +196,7 @@
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 12, 2017 00:00:00").getTime();
+var countDownDate = new Date("Oct 27, 2017 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
